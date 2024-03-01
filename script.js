a85efaa2cad4b6c784e70dc9646a340f439ffc4c
mudenise <157250431+mudenise@users.noreply.github.com>
2024-03-01 20:11:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 let intentos = 6;
-const PALABRA = "APPLE";
+let opciones = ["ANGEL", "PERRO", "TRAPO", "CINCO", "SIETE"];
+let winnerWord = opciones[Math.floor(Math.random() * opciones.length)];
+const GRID = document.getElementById("grid");
 
-document.getElementById("guess-button").addEventListener("click", ()=> {
+document.getElementById("guess-button").addEventListener("click", () => {
     const INTENTO = leerIntento();
-    if (INTENTO === PALABRA ) {
-        console.log("GANASTE!")
-        return
-    }
-    for (let i in PALABRA){
-        if (INTENTO[i]===PALABRA[i]){
-            console.log(INTENTO[i], "VERDE")
-        } else if( PALABRA.includes(INTENTO[i]) ) {
-            console.log(INTENTO[i], "AMARILLO")
-        } else {
-            console.log(INTENTO[i], "GRIS")
-        }
-    }
-		intentos--
-    if (intentos==0){
-        console.log("PERDISTE!")
-    }
-})
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
 
-function leerIntento(){
-    const INPUT= document.getElementById("guess-input");
-    const VALOR= INPUT.value.toUpperCase();
+    if (INTENTO===winnerWord){
+        terminar ("GANASTE");
+        return;
+    }
+    for (let i = 0; i < winnerWord.length; i++) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i] === winnerWord[i]) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if (winnerWord.includes(INTENTO[i])) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN);
+    }
+    GRID.appendChild(ROW);
+    
+    intentos--;
+    if (intentos === 0) {
+        terminar("PERDISTE");
+    } 
+    
+});
+
+function leerIntento() {
+    const INPUT = document.getElementById("guess-input");
+    const VALOR = INPUT.value.toUpperCase();
     return VALOR;
 }
 
-
-
+function terminar(mensaje) {
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    const BOTON= document.getElementById("guess-button");
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.style.display = 'block';
+    contenedor.innerHTML = mensaje;
+}
